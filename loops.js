89da590bcb1309e037a26d135efaed7b9e7847b2
Reetu Dave <rsdave5@gmail.com>
2019-10-28 23:24:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,11 @@ function whileLoop(n) {
   return 'done';
 }
 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(num) {
   do {
     console.log('I run once regardless.')
