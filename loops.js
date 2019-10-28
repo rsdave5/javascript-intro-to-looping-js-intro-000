1b147e29a77ccd74c0b82f72d8e53433dd9d0902
Reetu Dave <rsdave5@gmail.com>
2019-10-28 22:59:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 var forLoop = function(array) {
   for (let i = 0; i < 25; i++) {
-    if (i = 1) {
+    if (i === 1) {
       return `I am ${i} strange loop.`
     } else {
       return `I am ${i} strange loops.`
